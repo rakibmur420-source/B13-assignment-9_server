592b0b96882d25acc8ae822186dfc484d956577d
rakib <rakibmur420@gmail.com>
2026-06-15 22:17:21 +0600

    add facility

--- models/facility.model.js
+++ models/facility.model.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const facilitySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    facility_type: { type: String, required: true },
+    image: { type: String, required: true },
+    location: { type: String, required: true },
+    price_per_hour: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    available_slots: [{ type: String }],
+    description: { type: String, required: true },
+    owner_email: { type: String, required: true },
+    booking_count: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Facility", facilitySchema);
